5f485b50ffc93422e116f311443ad919c1fda7e8
maoyu.zhang <maoyu.zhang@tendcloud.com>
2017-03-17 09:49:54 +0800

    modify list to Array

--- main.go
+++ main.go
@@ -1,73 +1,84 @@
 package main
 
 import (
-	"github.com/figoxu/Figo"
-	"io/ioutil"
-	"fmt"
-	"github.com/quexer/utee"
-	"strings"
-	"regexp"
 	"flag"
-	"os"
+	"fmt"
+	"github.com/figoxu/Figo"
+	"github.com/quexer/utee"
+	"io/ioutil"
 	"log"
+	"os"
+	"regexp"
+	"strings"
 )
 
-func main(){
-	javaDir := *flag.String("input", "", "java dir for translate")
-	outfile := *flag.String("output", "", "output typescript filename")
-	files,_ := ioutil.ReadDir(javaDir)
+// ./Translate -input=/home/zmy/newsda/workspace/Translate/java -output=/home/zmy/newsda/workspace/Translate/ts/Campaign.ts
+func main() {
+	javaDir := flag.String("input", "", "java dir for translate")
+	outfile := flag.String("output", "", "output typescript filename")
+	flag.Parse()
+	files, _ := ioutil.ReadDir(*javaDir)
 	content := ""
-	for _,file := range files {
+	for _, file := range files {
 		if file.IsDir() {
 			continue
-		}else{
-			fname := fmt.Sprint(javaDir,"/",file.Name())
-			b,err:=ioutil.ReadFile(fname)
+		} else {
+			fname := fmt.Sprint(*javaDir, "/", file.Name())
+			fmt.Println("fname:", fname)
+			b, err := ioutil.ReadFile(fname)
 			utee.Chk(err)
-			name:=strings.Replace(file.Name(),".java","",-1)
-			code := parse2ts(string(b),name)
+			name := strings.Replace(file.Name(), ".java", "", -1)
+			code := parse2ts(string(b), name)
 			log.Println(code)
-			content = fmt.Sprintln(content,code)
+			content = fmt.Sprintln(content, code)
 		}
 	}
 	log.Println(content)
-	ioutil.WriteFile(outfile,[]byte(content),os.ModePerm)
+	ioutil.WriteFile(*outfile, []byte(content), os.ModePerm)
 }
 
-
-func parse2ts(content,name string) string{
+func parse2ts(content, name string) string {
 	parser := Figo.Parser{
-		PrepareReg:[]string{"private.+;"},
-		ProcessReg:[]string{".+serialVersionUID.+","private ",";"},
+		PrepareReg: []string{"private.+;"},
+		ProcessReg: []string{".+serialVersionUID.+", "private ", ";"},
 	}
 	results := parser.Exe(content)
-	result := fmt.Sprintln("class ",name,"{")
-	for _,v := range results {
-		vs := strings.Split(v," ");
-		result = fmt.Sprintln(result,"\t\t",vs[1],"\t:",parseJava2TsTp(vs[0]),";")
+	result := fmt.Sprintln("export class ", name, "{")
+	for _, v := range results {
+		vs := strings.Split(v, " ")
+		result = fmt.Sprintln(result, "\t\t", vs[1], "\t:", parseJava2TsTp(vs[0]), ";")
 	}
-	result = fmt.Sprintln(result,"}")
+	result = fmt.Sprintln(result, "}")
 	return result
 }
 
-func parseJava2TsTp(tp string)string{
-	tp =strings.ToLower(tp)
+func parseJava2TsTp(tp string) string {
+	tp = strings.ToLower(tp)
 	checkTp := func(reg string) bool {
+		log.Println("tp:", tp)
+		log.Println("reg:", reg)
 		rs := regexp.MustCompile(reg).FindAllString(tp, -1)
-		return len(rs)>0
+		return len(rs) > 0
 	}
 
-	if checkTp("bool"){
+	if checkTp("bool") {
 		return "boolean"
 	}
-	if checkTp("string"){
+	if checkTp("string") {
 		return "string"
 	}
-	if checkTp("int")||checkTp("long")||checkTp("double")||checkTp("float"){
-		return "string"
+	if checkTp("int") || checkTp("long") || checkTp("double") || checkTp("float") {
+		return "number"
 	}
-	if checkTp("date"){
+	if checkTp("date") {
 		return "Date"
 	}
-	return tp;
+	if checkTp("list") {
+		replace := strings.Replace(tp, "list", "Array", -1)
+		index := strings.Index(replace, "<")
+		upper := strings.ToUpper(replace[(index + 1):(index + 2)])
+		sprint := fmt.Sprint(replace[0:index+1], upper, replace[(index+2):len(replace)])
+		return sprint
+	}
+	return tp
 }
